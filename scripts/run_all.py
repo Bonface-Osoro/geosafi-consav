768e5c2a8fd933a3a39618cefd744237dfc335cc
Bonface-Osoro <bonfaceosoro@gmail.com>
2023-11-04 09:01:57 -0400

    poverty

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -24,24 +24,24 @@ countries = pd.read_csv(path, encoding = 'latin-1')
 
 for idx, country in countries.iterrows():
         
-    #if not country['region'] == 'Sub-Saharan Africa' or country['Exclude'] == 1:
+    if not country['region'] == 'Sub-Saharan Africa' or country['Exclude'] == 1:
         
-    if not country['iso3'] == 'KEN':
+    #if not country['iso3'] == 'KEN':
         
         continue 
 
     country = ProcessCountry(path, countries['iso3'].loc[idx])
-    #country.process_country_shapes()
+    country.process_country_shapes()
 
     regions = ProcessRegions(countries['iso3'].loc[idx], countries['lowest'].loc[idx])
-    #regions.process_regions()
-    #regions.process_sub_region_boundaries()
+    regions.process_regions()
+    regions.process_sub_region_boundaries()
 
     populations = ProcessPopulation(path, countries['iso3'].loc[idx], countries['lowest'].loc[idx], pop_tif_loc)
-    #populations.process_national_population()
-    #populations.process_population_tif()
-    #populations.process_sub_regional_pop_tiff()
-    #populations.pop_process_shapefiles()
+    populations.process_national_population()
+    populations.process_population_tif()
+    populations.process_sub_regional_pop_tiff()
+    populations.pop_process_shapefiles()
 
     poverty = PovertyProcess(path, countries['iso3'].loc[idx], countries['lowest'].loc[idx], poverty_shp)
     poverty.country_poverty()
